deef476633178bc5570180ff19a9fe6afc491198
Savelev <carry2002life@mail.ru>
2023-10-14 17:13:24 +0300

    Revert "front initial commit"
    This reverts commit 85f0d431866937bf0ec3a8255a79565e9c327309.

--- front/lib/global_variables/userdata.dart
+++
@@ -1,3 +0,0 @@
-class UserData {
-  static late int userId;
-}
--- front/lib/routes/routes.dart
+++
@@ -1,5 +0,0 @@
-class Routes {
-  static String storage = '/storage';
-  static String register = '/register';
-  static String chat = '/chat';
-}
